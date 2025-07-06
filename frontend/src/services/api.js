dcaf02f15c5516b488e5934d56e11b4e21e61fa6
Diwakar chaurasiya <diwakarbmj1212@gmail.com>
2025-07-06 12:42:27 +0530

    updated frontend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://protender-backend.onrender.com/";
+const API_URL = "https://protender-backend.onrender.com/api" || 'http://localhost:5000/api'
 
 class ApiService {
   constructor() {
